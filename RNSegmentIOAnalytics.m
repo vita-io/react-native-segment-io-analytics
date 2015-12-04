a963b6855025b28b2aa74327b8bcbb456ef5e191
Tal Kain <tal@kain.net>
2015-12-04 21:48:47 +0200

    added exported functions
    Signed-off-by: Tal Kain <tal@kain.net>

--- RNSegmentIOAnalytics.m
+++ RNSegmentIOAnalytics.m
@@ -9,6 +9,17 @@ RCT_EXPORT_MODULE()
 RCT_EXPORT_METHOD(test)
 {
   // Your implementation here
+    NSLog(@"Called 'test' function");
+}
+
+RCT_EXPORT_METHOD(setup: (NSString*)configKey)
+{
+    //[SEGAnalytics setupWithConfiguration:[SEGAnalyticsConfiguration configurationWithWriteKey:@"YOUR_WRITE_KEY"]];
+    [SEGAnalytics setupWithConfiguration:[SEGAnalyticsConfiguration configurationWithWriteKey:configKey]];
+}
+
+RCT_EXPORT_METHOD(identifyUser: (NSString*)userId withName:(NSString*)name withEmail:(NSString*)email) {
+    [[SEGAnalytics sharedAnalytics] identify:userId traits:@{ @"name": name, @"email": email }];
 }
 
 @end
