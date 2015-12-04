8c4f8676daae11786cf7639817560e2c142115e3
Tal Kain <tal@kain.net>
2015-12-04 23:04:11 +0200

    refactoring
    Signed-off-by: Tal Kain <tal@kain.net>

--- RNSegmentIOAnalytics.m
+++ RNSegmentIOAnalytics.m
@@ -12,43 +12,39 @@ RCT_EXPORT_METHOD(test)
     NSLog(@"Called 'test' function");
 }
 
-RCT_EXPORT_METHOD(setup: (NSString*)configKey)
+RCT_EXPORT_METHOD(setup:(NSString*)configKey)
 {
     NSLog(@"Setting up RNSegmentIOAnalytics using key=%@", configKey);
     [SEGAnalytics setupWithConfiguration:[SEGAnalyticsConfiguration configurationWithWriteKey:configKey]];
 }
 
-RCT_EXPORT_METHOD(identifyUser: (NSString*)userId traits:(NSDictionary *)traits) {
+RCT_EXPORT_METHOD(identifyUser:(NSString*)userId traits:(NSDictionary *)traits) {
     /*
      According to React Native's documentation:
      
      For maps, it is the developer's responsibility to check the value types individually by manually calling RCTConvert helper methods.
      */
     NSLog(@"identifyUser: identifying user (id=%@)", userId);
-    NSDictionary* stringsTraits = [NSDictionary new];
-    if (traits != nil) {
-        for (NSString* key in [traits allKeys]) {
-            [stringsTraits setValue:traits[key] forKey:key];
-        }
-    }
-
-    [[SEGAnalytics sharedAnalytics] identify:userId traits:stringsTraits];
+    [[SEGAnalytics sharedAnalytics] identify:userId traits:[self convertToStringDictionary:traits]];
 }
 
-RCT_EXPORT_METHOD(track: (NSString*)trackText properties:(NSDictionary *)properties) {
+RCT_EXPORT_METHOD(track:(NSString*)trackText properties:(NSDictionary *)properties) {
+    [[SEGAnalytics sharedAnalytics] track:trackText
+                               properties:[self convertToStringDictionary:properties]];
+}
+
+-(NSMutableDictionary*) convertToStringDictionary: (NSDictionary *)properties {
     /*
      According to React Native's documentation:
      
      For maps, it is the developer's responsibility to check the value types individually by manually calling RCTConvert helper methods.
      */
-    NSDictionary* stringsProperties = [NSDictionary new];
-    if (properties != nil) {
-        for (NSString* key in [properties allKeys]) {
-            [stringsProperties setValue:properties[key] forKey:key];
-        }
+    NSMutableDictionary *stringDictionary = [[NSMutableDictionary alloc] init];
+    for (NSString* key in [properties allKeys]) {
+        id value = [RCTConvert NSString:[properties objectForKey:key]];
+        [stringDictionary setObject:value forKey:[RCTConvert NSString:key]];
     }
-    [[SEGAnalytics sharedAnalytics] track:trackText
-                               properties:stringsProperties];
+    return stringDictionary;
 }
 
 @end
