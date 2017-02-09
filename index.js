987533d3a8ed29bbad1735ab90c013fc8e4ffad5
Ruben Weijers <ruben@vita.io>
2017-02-09 14:15:58 +0100

    Revert "fix nr of parameters for setup call"
    This reverts commit 623749b53f9bc42f09b8bc9efb7db3f9eb3e6a8c.

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ var RNSegmentIOAnalytics = {
      * @param flushAt https://segment.com/docs/libraries/ios/#flushing or https://segment.com/docs/libraries/android/#customizing-the-client
      * @param shouldUseLocationServices https://segment.com/docs/libraries/ios/#location-services
      */
-    setup: function (configKey, flushAt = 20, shouldUseLocationServices = false) {
-        NativeRNSegmentIOAnalytics.setup(configKey, flushAt, shouldUseLocationServices);
+    setup: function (configKey, flushAt = 20, shouldUseLocationServices = false, debug = false) {
+        NativeRNSegmentIOAnalytics.setup(configKey, flushAt, shouldUseLocationServices, debug);
     },
 
     /*
@@ -34,7 +34,7 @@ var RNSegmentIOAnalytics = {
     identifyUser: function (userId, traits) {
         NativeRNSegmentIOAnalytics.identifyUser(userId, traits);
     },
-
+    
     /*
      * https://segment.com/docs/libraries/ios/#alias
      */
